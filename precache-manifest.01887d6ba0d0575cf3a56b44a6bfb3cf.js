ed19ea2e428fe691d121f677c0503a2f8fbc7883
Nakamura <n.aoi.pg@gmail.com>
2020-07-03 23:15:14 +0900

    Updates

---
+++ precache-manifest.01887d6ba0d0575cf3a56b44a6bfb3cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b61cfda2a01499e6f63870816042c44",
+    "url": "/impl/index.html"
+  },
+  {
+    "revision": "7c5c62b44c1931bfe95b",
+    "url": "/impl/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "39f35784dc3b71c28050",
+    "url": "/impl/static/js/2.c231233b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/impl/static/js/2.c231233b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5c62b44c1931bfe95b",
+    "url": "/impl/static/js/main.79cfa3ef.chunk.js"
+  },
+  {
+    "revision": "e6184d6718ab6321e6c2",
+    "url": "/impl/static/js/runtime-main.5fc04746.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/impl/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/impl/precache-manifest.01887d6ba0d0575cf3a56b44a6bfb3cf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/impl/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
